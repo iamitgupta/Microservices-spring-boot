6ae508920450a9c2576444ba5f8b66074023c2f7
iamitgupta <amitgpt840@gmail.com>
2023-04-11 17:04:18 +0530

    added config server and circuit breaker

--- Order-Service/src/main/java/com/os/controller/OrderController.java
+++ Order-Service/src/main/java/com/os/controller/OrderController.java
@@ -7,11 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.os.dto.OrderReqDTO;
-import com.os.model.Order;
 import com.os.services.OrderService;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 @RestController
 @RequestMapping("/order")
@@ -28,8 +32,14 @@ public class OrderController {
 	
 	
 	@PostMapping("/place")
+    @CircuitBreaker(name = "product", fallbackMethod = "productFallback")
 	public ResponseEntity<Object> placeOrder(@RequestBody OrderReqDTO order){
 		return ResponseEntity.status(HttpStatus.SC_CREATED).body(service.placeOrder(order));
 	}
+	
+	public ResponseEntity<String> productFallback(OrderReqDTO order, RuntimeException runtimeException) {
+		return ResponseEntity.status(HttpStatus.SC_OK).body("Oops! Something went wrong, please retry later!");
+	}
+	
 
 }
